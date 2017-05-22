a86f150b32df701348837c6154123d55fcbde11a
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2017-05-22 15:37:03 -0300

    Created imports of components

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import TextComponent from './text/text.component';
+import ButtonComponent from './button/button.component';
+import UploadComponent from './upload/upload.component';
+import ImageComponent from './image/image.component';
+import MapComponent from './map/map.component';
+
+export {
+    TextComponent,
+    ButtonComponent,
+    UploadComponent,
+    ImageComponent,
+    MapComponent
+};
